c357e7d16a0381e0a45cc0c29731dd675f0603b7
Dmitry Baryshnikov <bishop.dev@gmail.com>
2015-03-17 02:01:12 +0800

    Add line edit view

--- app/src/main/java/com/nextgis/mobile/MapFragment.java
+++ app/src/main/java/com/nextgis/mobile/MapFragment.java
@@ -559,7 +559,8 @@ public class MapFragment
 
     protected void addNewGeometry(){
         //show select layer dialog if several layers, else start default or custom form
-        List<ILayer> layers = mMap.getVectorLayersByType(GeoConstants.GTPointCheck | GeoConstants.GTMultiPointCheck);
+        List<ILayer> layers = mMap.getVectorLayersByType(GeoConstants.GTPointCheck |
+        GeoConstants.GTMultiPointCheck | GeoConstants.GTLineStringCheck | GeoConstants.GTPolygonCheck);
         if(layers.isEmpty()){
             Toast.makeText(getActivity(), getString(R.string.warning_no_edit_layers), Toast.LENGTH_LONG).show();
         }
