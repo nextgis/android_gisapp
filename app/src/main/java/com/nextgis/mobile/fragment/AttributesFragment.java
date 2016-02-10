2f1e2b5cc808e5bb6197ac25e0432e774b3b18d7
Stanislav Petryakov <become.glory@yandex.ru>
2016-02-10 18:22:12 +0300

    Fix #254

--- app/src/main/java/com/nextgis/mobile/fragment/AttributesFragment.java
+++ app/src/main/java/com/nextgis/mobile/fragment/AttributesFragment.java
@@ -5,7 +5,7 @@
  * Author:   NikitaFeodonit, nfeodonit@yandex.com
  * Author:   Stanislav Petriakov, becomeglory@gmail.com
  * *****************************************************************************
- * Copyright (c) 2012-2015. NextGIS, info@nextgis.com
+ * Copyright (c) 2012-2016 NextGIS, info@nextgis.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -199,7 +199,7 @@ public class AttributesFragment
         if (null == activity)
             return;
 
-        ((MainActivity) activity).setSubtitle(String.format(getString(R.string.of), mItemPosition + 1, mFeatureIDs.size()));
+        ((MainActivity) activity).setSubtitle(String.format(getString(R.string.features_count_attributes), mItemPosition + 1, mFeatureIDs.size()));
 
         String selection = Constants.FIELD_ID + " = ?";
         Cursor attributes = mLayer.query(null, selection, new String[]{mItemId + ""}, null, null);
