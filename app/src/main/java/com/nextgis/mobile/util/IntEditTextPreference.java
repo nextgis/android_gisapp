41280129af4327e07f4f95d94bb5452faff4a8a2
Stanislav Petryakov <become.glory@yandex.ru>
2016-02-15 16:58:53 +0300

    Missed preference class

---
+++ app/src/main/java/com/nextgis/mobile/util/IntEditTextPreference.java
@@ -0,0 +1,50 @@
+/*
+ * Project:  NextGIS Mobile
+ * Purpose:  Mobile GIS for Android.
+ * Author:   Stanislav Petriakov, becomeglory@gmail.com
+ * *****************************************************************************
+ * Copyright (c) 2016 NextGIS, info@nextgis.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.nextgis.mobile.util;
+
+import android.content.Context;
+import android.preference.EditTextPreference;
+import android.util.AttributeSet;
+
+public class IntEditTextPreference extends EditTextPreference {
+	public IntEditTextPreference(Context context) {
+		super(context);
+	}
+
+	public IntEditTextPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public IntEditTextPreference(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public String getPersistedString(String defaultReturnValue) {
+		return String.valueOf(getPersistedInt(-1));
+	}
+
+	@Override
+	public boolean persistString(String value) {
+		return persistInt(Integer.valueOf(value));
+	}
+}
