9d8a5b516f51016074ee7f8387a05df160dc5ec6
Stanislav Petryakov <become.glory@yandex.ru>
2016-07-25 17:12:40 +0300

    Add prompt dialog on exit in ModifyAttributesActivity

--- app/src/main/java/com/nextgis/mobile/fragment/LayersFragment.java
+++ app/src/main/java/com/nextgis/mobile/fragment/LayersFragment.java
@@ -278,7 +278,7 @@ public class LayersFragment
                                 mapFragment.setMode(MapFragment.MODE_NORMAL);
                             }
                         })
-                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        .setNegativeButton(R.string.discard, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 mapFragment.cancelEdits();
