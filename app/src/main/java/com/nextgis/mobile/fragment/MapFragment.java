6e116d79fec3f031f6eaaed4e9d71c93f4b81149
Stanislav Petryakov <become.glory@yandex.ru>
2016-10-05 12:37:49 +0300

    Fix save button inactive on cancel attributes

--- app/src/main/java/com/nextgis/mobile/fragment/MapFragment.java
+++ app/src/main/java/com/nextgis/mobile/fragment/MapFragment.java
@@ -294,8 +294,10 @@ public class MapFragment
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (mMode == MODE_INFO || resultCode != Activity.RESULT_OK)
+        if (mMode == MODE_INFO || resultCode != Activity.RESULT_OK) {
+            mEditLayerOverlay.setHasEdits(true);
             return;
+        }
 
         if (requestCode == IVectorLayerUI.MODIFY_REQUEST && data != null) {
             long id = data.getLongExtra(ConstantsUI.KEY_FEATURE_ID, NOT_FOUND);
