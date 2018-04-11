83c67216b42865143c019c8eac73f9b31520692f
Stanislav Petryakov <become.glory@yandex.ru>
2018-04-11 13:18:38 +0300

    Set first point to current geolocation, fix #598

--- app/src/main/java/com/nextgis/mobile/fragment/MapFragment.java
+++ app/src/main/java/com/nextgis/mobile/fragment/MapFragment.java
@@ -5,7 +5,7 @@
  * Author:   NikitaFeodonit, nfeodonit@yandex.com
  * Author:   Stanislav Petriakov, becomeglory@gmail.com
  * *****************************************************************************
- * Copyright (c) 2012-2017 NextGIS, info@nextgis.com
+ * Copyright (c) 2012-2018 NextGIS, info@nextgis.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -1899,7 +1899,7 @@ public class MapFragment
     }
 
     protected void startMeasuring() {
-        mRulerOverlay.startMeasuring(this);
+        mRulerOverlay.startMeasuring(this, mCurrentCenter);
         hideOverlayPoint();
         hideMainButton();
         hideRulerButton();
