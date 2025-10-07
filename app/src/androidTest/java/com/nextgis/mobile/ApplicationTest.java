866deafbfc15bbaa2e5addcb102bd1f83a524a8e
Aleksei Kovalenko <alexey.kovalenko.v@gmail.com>
2025-10-08 09:47:11 +1000

    fix opening form after update app

--- app/src/androidTest/java/com/nextgis/mobile/ApplicationTest.java
+++ app/src/androidTest/java/com/nextgis/mobile/ApplicationTest.java
@@ -24,17 +24,16 @@
 package com.nextgis.mobile;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
 
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest
-        extends ApplicationTestCase<Application>
+
 {
     public ApplicationTest()
     {
-        super(Application.class);
+        ;
     }
 }
